4e79eb9185572ae7488d0653f4b85d1004823b8f
salman <nmssalman@outlook.com>
2020-07-22 03:16:20 +0900

    init

---
+++ app/src/main/java/nmssalman/global/cicdsample/MainActivity.kt
@@ -0,0 +1,13 @@
+package nmssalman.global.cicdsample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        textVersion.setText("Version Name: " + BuildConfig.VERSION_NAME)
+    }
+}
